23870ee5909899319854a30fadbd4fc26136035d
Katia Rodrigues <krodrigues.quimica@gmail.com>
2022-01-19 11:51:10 -0300

    primeiro commit

---
+++ Teste/TesteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesteApp.swift
+//  Teste
+//
+//  Created by Katia Cristina Rodrigues on 18/01/22.
+//
+
+import SwiftUI
+
+@main
+struct TesteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
